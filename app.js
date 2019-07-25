706df4c6be0a9c84f5cbdd9688410da2181103e5
Michael Villarreal <mvillarreal@bellicum.com>
2019-07-25 15:45:04 -0500

    progress

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require("express");
       bodyParser = require("body-parser");
       app = express();
 
-mongoose.connect("mongodb+srv://mikev8492:0125@surfblog-dnpkw.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://mikev8492:1234@surfblog-dnpkw.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//MONGOOSE MODEL CONFIGURATION
 const blogSchema = new mongoose.Schema({
   title: String,
   image: String,
@@ -16,24 +18,25 @@ const blogSchema = new mongoose.Schema({
 const Blog = mongoose.model("Blog", blogSchema);
 
 //MANUALLY INPUTS SOME STARTER DATA
-Blog.create(
-  {
-    title: "Gulf Coast",
-    image: "https://images.unsplash.com/photo-1502209944943-85c1d6146367?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-    body: "Blah blah blah"
-  }, function(err, blogs){
-      if (err) {
-        console.log(err)
-      } else {
-        console.log("Newly created blog: ");
-        console.log(blogs);
-      }
-  });
+// Blog.create(
+//   {
+//     title: "Gulf Coast",
+//     image: "https://images.unsplash.com/photo-1502209944943-85c1d6146367?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+//     body: "Blah blah blah"
+//   }, function(err, blogs){
+//       if (err) {
+//         console.log(err)
+//       } else {
+//         console.log("Newly created blog: ");
+//         console.log(blogs);
+//       }
+//   });
 
 app.get("/", function(req, res){
   res.redirect("/surf");
 });
 
+//INDEX ROUTE
 app.get("/surf", function(req, res){
   // GET ALL CAMPGROUNDS FROM THE DB
   Blog.find({}, function(err, allBlogs){
@@ -43,6 +46,11 @@ app.get("/surf", function(req, res){
       res.render("index", {blogs: allBlogs});
     }
   });
+});
+
+//NEW ROUTE
+app.get("/surf/new", function(req, res){
+  res.render("new");
 });
 
 app.listen(3000, function(){
